42fd9ff77369f5046b37a8ae89ab785041a9a52b
Joydeep Pandey <87060928+ShinraTenseiiiii@users.noreply.github.com>
2025-02-21 13:09:14 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -52,8 +52,23 @@ export default function Page() {
                 <RandomQuote />
                 <Markdown content={postDynamicContentExplainer} />
             </section>
+            
             {/* !!cards?.length && <CardsGrid cards={cards} /> */}
         </main>
+             <script
+                type="application/javascript"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        (function(w,d,s,o,f,js,fjs){
+                            w[o]=w[o]||function(){(w[o].q=w[o].q||[]).push(arguments);};
+                            (js=d.createElement(s)),(fjs=d.getElementsByTagName(s)[0]);
+                            js.id=o; js.src=f; js.async=1; js.referrerPolicy = "origin";
+                            fjs.parentNode.insertBefore(js,fjs);
+                        })(window,document,"script","copilot","https://script.copilot.live/v1/copilot.min.js?tkn=cat-rhq1rrp3");
+                        copilot("init",{});
+                    `,
+                }}
+            />
     );
 }
 
